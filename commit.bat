ebec0a44c1e13b13125b7641b1d7f7d7dfc0bc39
StoryVerse <deploy@storyverse.app>
2026-06-16 05:49:40 +0700

    remove-commit-bat

--- commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add "app/story/[id]/page.tsx" "app/write/[id]/page.tsx" "components/Providers.tsx"
-git commit -m "fix: dark mode persist and isAuthor computed"
-git push origin main
-del commit.bat
